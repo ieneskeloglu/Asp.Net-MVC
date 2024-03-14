39745d83cbecd5400185fc46115f13acc0f83e83
ienes <ienes@DESKTOP-27C0RU6>
2024-03-14 23:05:57 +0300

    Add project files.

---
+++ MyPortfolio1/Controllers/AboutController.cs
@@ -0,0 +1,20 @@
+﻿using MyPortfolio1.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Remoting.Metadata.W3cXsd2001;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MyPortfolio1.Controllers
+{
+    public class AboutController : Controller
+    {
+        MyAcademyPortfolioProjectEntities db=new MyAcademyPortfolioProjectEntities();
+         public ActionResult Index()
+        {
+            var values=db.TblAbouts.ToList();
+            return View(values);
+        }
+    }
+}
---
+++ MyPortfolio1/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="MyPortfolio1.MvcApplication" Language="C#" %>
